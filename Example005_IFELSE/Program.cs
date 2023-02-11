63c94de993d982f3a6917a6d6572d8c4bed498fd
diablo332 <diablo332@mail.ru>
2023-02-11 12:57:16 +0600

    Example005 IF ELSE

---
+++ Example005_IFELSE/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "Маша".ToLower()){
+    Console.WriteLine("Ура, это же Маша!");
+} else 
+{
+    Console.WriteLine("Привет, " + userName);
+}
