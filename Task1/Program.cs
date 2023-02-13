66c08fa0f26cfc6516f6bf1d624802714a4ea528
diablo332 <diablo332@mail.ru>
2023-02-13 21:27:06 +0600

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿int x = 5;
+int y = 3;
+
+Console.WriteLine("Max = " + MaxOutputConsole(x, y));
+Print("Hello");
+int cti = ConsoleTakeInt();
+
+Print(cti.ToString());
+
+int ConsoleTakeInt()
+{
+    int number;
+    Console.Write($"Введите число: ");
+    bool flag = int.TryParse(Console.ReadLine()!, out number);
+    if (flag){
+        return number;
+    }  else {
+        Console.Write($"Введено не число\n");
+        return ConsoleTakeInt();        
+    } 
+}
+
+int MaxOutputConsole(int a, int b){
+    int max = a;
+    if (a > b) {
+        max = a;
+    } else {
+        max = b;
+    }
+    return max;
+}
+
+void Print(string text){
+    int length = text.Length;
+    string border = "--";
+    string textt = "|" + text + "|";
+    for (int i = 0; i < length; i++)
+        border += "-";
+    Console.Write($"{border}\n{textt}\n{border}\n");    
+
+}
+
+/*void MaxOutputConsole(int a, int b){
+    if (a > b) {
+        Console.Write("Max = a");
+    } else {
+        Console.Write("Max = b");
+    }
+}*/
