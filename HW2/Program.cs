871a7ff807648fc4c8a67cf880d1680afde7b4d9
diablo332 <diablo332@mail.ru>
2023-02-16 19:12:39 +0600

    HW2

---
+++ HW2/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+int getThirthNumber(){
+    int getNumber(){
+        int number;
+        Console.Write($"Введите число:\n");
+        bool flag = int.TryParse(Console.ReadLine()!, out number);
+        if (flag){
+            return number;
+        }  else {
+            Console.Write($"Введите число:\n");
+            return getNumber();        
+        }
+    }
+
+    int number = getNumber();
+    if (number >=100){
+        while (number >= 1000){
+            number = number / 10;
+        }
+        int thirthNumber = number % 10;
+        Console.Write($"Третья цифра:" + thirthNumber.ToString());
+        return thirthNumber;
+    } else {
+        Console.Write($"Вы ввели не трёхзначное число\n");    
+        return getThirthNumber();
+    }  
+}
+getThirthNumber(); 
