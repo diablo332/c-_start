0ed3686e99384da6e6a4f08eed48a9d295662992
Vladimir Grebenshchikov <diablo332@mail.ru>
2023-02-22 14:44:26 +0600

    Third task

--- seminar3/Program.cs
+++ seminar3/Program.cs
@@ -1,9 +1,28 @@
-﻿/* Напишите программу, которая по заданному номеру четверти показывает диапазон возможных точек в этой четверти (X и Y)*/
+﻿/* Напишите программу, которая принимает на вход координаты двух точек и
+находит расстояние между ними в 2D плоскости.
+A (3, 6); B (2, 1) -> 5, 09
+A (7, -5); B (1, -1) -> 7, 21*/
 
-int n;
-Console.Write($"Введите номер плоскости: ");
-int.TryParse(Console.ReadLine()!, out n);
+double[] A = new double[2];
+double[] B = new double[2];
+Console.Write($"Введите координату x точки А: ");
+double.TryParse(Console.ReadLine()!, out A[0]);
 
+Console.Write($"Введите координату y точки A: ");
+double.TryParse(Console.ReadLine()!, out A[1]);
+
+Console.Write($"Введите координату x точки B: ");
+double.TryParse(Console.ReadLine()!, out B[0]);
+
+Console.Write($"Введите координату y точки B: ");
+double.TryParse(Console.ReadLine()!, out B[1]);
+
+double Xdist = Math.Pow(A[0] - B[0], 2);
+double Ydist = Math.Pow(A[1] - B[1], 2);
+double ans = Math.Round(Math.Sqrt(Xdist + Ydist), 2);
+
+Console.Write($"A({A[0]};{A[1]}) B({B[0]};{B[1]}) -> {ans}");
+/*
 switch (n)
 {
     case 1: Console.Write($"X>0 Y>0");
@@ -20,7 +39,7 @@ switch (n)
     default: 
     Console.Write($"Неверно введён номер плоскости");
     break;
-}
+}*/
 
 /* if (n == 1)
     Console.Write($"X>0 Y>0");
