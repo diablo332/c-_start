d9727e35ed927bfd2b544f757c93728b799239d2
Vladimir Grebenshchikov <diablo332@mail.ru>
2023-02-22 12:23:16 +0600

    First task

---
+++ seminar3/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, которая принимает на вход координаты точки (X и Y),
+причём X <> 0 и Y <> 0 и выдаёт номер четверти плоскости, в которой находится эта точка */
+
+int x,y;
+Console.Write($"Введите x: ");
+int.TryParse(Console.ReadLine()!, out x);
+Console.Write($"Введите y: ");
+int.TryParse(Console.ReadLine()!, out y);
+
+if (x > 0)
+{
+    if (y > 0)
+    {
+        Console.Write($"Плоскость 1");
+    } else {
+        Console.Write($"Плоскость 4");
+    }
+} else {
+    if (y > 0)
+    {
+        Console.Write($"Плоскость 2");
+    } else {
+        Console.Write($"Плоскость 3");
+    }
+}
