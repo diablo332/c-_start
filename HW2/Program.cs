678e41dcd5e4dbc6d676d321b5adbbfc41a76983
diablo332 <diablo332@mail.ru>
2023-02-26 19:40:02 +0600

    seminar6

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -1,27 +1,22 @@
-﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. 
-Найдите сумму элементов, стоящих на нечётных позициях.
-[3, 7, 23, 12] -> 19
-[-4, -6, 89, 6] -> 0 */
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+double b1 = 0;
+Console.WriteLine("Введите значение b1");
+double.TryParse(Console.ReadLine(), out b1);
 
-int[] array = FillArray(5, 10, 99);
-Console.Write($"{String.Join(", ", array)}, -> {getOddIndexesSum(array)}\n");
+double k1 = 0;
+Console.WriteLine("Введите значение k1");
+double.TryParse(Console.ReadLine(), out k1);
 
-int getOddIndexesSum(int[] a){
-    int sum = 0;
-    int i = 1;
-    while (i <= a.Length -1){
-        sum += a[i];
-        i += 2;
-    }
-    return sum;
-}
+double b2 = 0;
+Console.WriteLine("Введите значение b2");
+double.TryParse(Console.ReadLine(), out b2);
 
-int[] FillArray(int size, int min = 0, int max = 10)
-{
-    int[] arr = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        arr.SetValue(new Random().Next(min, max), i);
-    }
-    return arr;
-}
+double k2 = 0;
+Console.WriteLine("Введите значение k2");
+double.TryParse(Console.ReadLine(), out k2);
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"Прямые пересекутся в точке с координатами X: {x}, Y: {y}");
