7a5237c2594cd19738ca33e0efd3a3e7f923bad4
diablo332 <diablo332@mail.ru>
2023-03-04 19:53:01 +0600

    lesson 7

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -1,38 +1,36 @@
-﻿/* Задача 38: Задайте массив вещественных чисел. 
-Найдите разницу между максимальным и минимальным элементов массива.
-[3 7 22 2 78] -> 76 */
+﻿/* Задача 52. Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
 
-int[] array = FillArray(5, 10, 99);
-Console.Write($"{String.Join(", ", array)}, -> {getMax(array) - getMin(array)}\n");
-
-int getMax(int[] a){
-    int max = a[0];
-    for (int i = 1; i < a.Length; i++)
-    {
-        if (a[i] > max){
-            max = a[i];
-        }
-    }
-    return max;
-}
-
-int getMin(int[] a){
-    int min = a[0];
-    for (int i = 1; i < a.Length; i++)
-    {
-        if (a[i] < min){
-            min = a[i];
-        }
-    }
-    return min;
-}
-
-int[] FillArray(int size, int min = 0, int max = 10)
+int[,] array = new int[3, 4];
+array = FillArray(array);
+double avg = 0;
+Console.Write("Среднее арифметическое каждого столбца: ");
+for (int j = 0; j <= array.GetLength(1) - 1; j++)
 {
-    int[] arr = new int[size];
-    for (int i = 0; i < size; i++)
+    avg = 0;
+    for (int i = 0; i <= array.GetLength(0) - 1; i++)
     {
-        arr.SetValue(new Random().Next(min, max), i);
+        avg += array[i, j];
+    }
+    avg = avg / array.GetLength(0);
+    Console.Write($"{Math.Round(avg, 1)}; ");
+}
+
+int[,] FillArray(int[,]  arr)
+{
+    for (int i = 0; i <= array.GetLength(0) - 1; i++)
+    {
+        for (int j = 0; j <= array.GetLength(1) - 1; j++)
+        {
+            array[i, j] = new Random().Next(10, 100);
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
     }
     return arr;
 }
